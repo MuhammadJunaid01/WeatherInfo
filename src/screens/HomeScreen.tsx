c861b6e4dd78d5835e1d5012dd7534541745e2fb
MuhammadJunaid01 <m.junaidbkh2020@gmail.com>
2025-02-03 00:18:12 +0600

    Add theme toggle buttons to HomeScreen for dark and light mode switching

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
-import {StatusBar} from 'react-native';
+import {Button, StatusBar} from 'react-native';
 import tw from '../../tailwind';
 import {ThemedText, ThemedView} from '../components';
+import {useAppDispatch} from '../hooks/useReduxHooks';
+import {setTheme} from '../services/features/themeSlice';
 
 const HomeScreen = () => {
+  const dispatch = useAppDispatch();
   return (
-    <ThemedView style={tw` flex-1  p-3 `}>
+    <ThemedView style={tw` flex-1  p-3  gap-y-3`}>
       <StatusBar barStyle={'dark-content'} backgroundColor={'white'} />
       <ThemedText size="h2">Home</ThemedText>
+      <Button title="Dark Mode" onPress={() => dispatch(setTheme('dark'))} />
+      <Button title="Light Mode" onPress={() => dispatch(setTheme('light'))} />
+      <Button title="Dark Mode" onPress={() => dispatch(setTheme('system'))} />
     </ThemedView>
   );
 };
