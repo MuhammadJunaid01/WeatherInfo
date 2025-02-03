29aeef3a8d16d9752049d38810dffff5f633a311
MuhammadJunaid01 <m.junaidbkh2020@gmail.com>
2025-02-03 18:40:49 +0600

    Implement dynamic theming in TabNavigator for improved user experience

--- src/navigation/TabNavigator.tsx
+++ src/navigation/TabNavigator.tsx
@@ -1,69 +1,84 @@
 /* eslint-disable react/no-unstable-nested-components */
-// TabNavigator.tsx
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import React from 'react';
 import {HomeIcon, NewsIcon, WeatherIcon} from '../assets/icons';
 import {COLORS, moderateVerticalScale, scale} from '../config/constants';
+import {useAppSelector} from '../hooks/useReduxHooks';
 import {TabParamList} from '../lib/types';
 import {HomeScreen, NewsScreen, WeatherInfoScreen} from '../screens';
 
 const Tab = createBottomTabNavigator<TabParamList>();
 
-const TabNavigator = () => (
-  <Tab.Navigator
-    screenOptions={{
-      headerShown: false,
-      tabBarActiveTintColor: `${COLORS.primary}`,
-      tabBarLabelStyle: {
-        fontSize: scale(11), // Adjust font size
-        fontWeight: '600', // Optional: Make text bold
-        paddingTop: 3,
-      },
-      tabBarStyle: {
-        height: moderateVerticalScale(55),
-        justifyContent: 'center',
-        alignItems: 'center',
-        paddingTop: scale(10),
-        paddingBottom: scale(10),
-      },
-    }}>
-    <Tab.Screen
-      name="Home"
-      options={{
-        tabBarIcon: ({focused}) => {
-          return (
-            <HomeIcon size={40} fill={focused ? COLORS.primary : '#6b7280'} />
-          );
+const TabNavigator = () => {
+  const {theme} = useAppSelector(state => state.theme);
+
+  // Dynamic styles based on theme
+  const isDarkTheme = theme === 'dark';
+  const tabBarBackgroundColor = isDarkTheme
+    ? COLORS.dark.primary
+    : COLORS.light.primary;
+  const tabBarIconColor = isDarkTheme
+    ? COLORS.dark.secondary
+    : COLORS.light.secondary;
+
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: COLORS.primary, // Active icon color
+        tabBarInactiveTintColor: tabBarIconColor, // Inactive icon color
+        tabBarLabelStyle: {
+          fontSize: scale(11),
+          fontWeight: '600',
+          paddingTop: 3,
         },
-      }}
-      component={HomeScreen}
-    />
-    <Tab.Screen
-      name="News"
-      component={NewsScreen}
-      options={{
-        tabBarIcon: ({focused}) => {
-          return (
-            <NewsIcon size={40} fill={focused ? COLORS.primary : '#6b7280'} />
-          );
+        tabBarStyle: {
+          height: moderateVerticalScale(55),
+          justifyContent: 'center',
+          alignItems: 'center',
+          paddingTop: scale(10),
+          paddingBottom: scale(10),
+          backgroundColor: tabBarBackgroundColor, // Dynamic background color
         },
-      }}
-    />
-    <Tab.Screen
-      name="Weather"
-      component={WeatherInfoScreen}
-      options={{
-        tabBarIcon: ({focused}) => {
-          return (
+      }}>
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({focused}) => (
+            <HomeIcon
+              size={40}
+              fill={focused ? COLORS.primary : tabBarIconColor}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="News"
+        component={NewsScreen}
+        options={{
+          tabBarIcon: ({focused}) => (
+            <NewsIcon
+              size={40}
+              fill={focused ? COLORS.primary : tabBarIconColor}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Weather"
+        component={WeatherInfoScreen}
+        options={{
+          tabBarIcon: ({focused}) => (
             <WeatherIcon
               size={40}
-              fill={focused ? COLORS.primary : '#6b7280'}
+              fill={focused ? COLORS.primary : tabBarIconColor}
             />
-          );
-        },
-      }}
-    />
-  </Tab.Navigator>
-);
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
 
 export default TabNavigator;
